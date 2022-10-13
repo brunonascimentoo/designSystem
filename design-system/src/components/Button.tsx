f80fb1c7d1b20abd0028860afc3379d4aa7abae2
Bruno Nascimento <bnascimentos1996@gmail.com>
2022-10-12 22:13:48 -0300

    componente button

---
+++ design-system/src/components/Button.tsx
@@ -0,0 +1,24 @@
+import { clsx} from 'clsx';
+import { Slot } from '@radix-ui/react-slot';
+import { ReactNode } from 'react';
+
+export interface ButtonProps {
+  children: ReactNode;
+  asChild?: boolean;
+} 
+
+export function Button({ children, asChild }: ButtonProps) {
+
+  const Comp = asChild ? Slot : 'button';
+
+  return (
+    <Comp
+      className={clsx(
+      'text-gray-100 font-sans', 
+
+    )}
+    >
+      {children}
+    </Comp>
+  )
+}

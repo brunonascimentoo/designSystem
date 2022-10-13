aaf5d1a0c2482bb31df6b4a5e78a740e3606bdfe
Bruno Nascimento <bnascimentos1996@gmail.com>
2022-10-12 21:37:23 -0300

    componente texto finalizado + radix slot

---
+++ design-system/src/components/Text.stories.tsx
@@ -0,0 +1,34 @@
+import { Text, TextProps } from './Text'
+import { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'components/Text',
+  component: Text,
+  args: {
+    children: 'Lorem ipsum',
+  },
+
+} as Meta<TextProps>
+
+export const Default: StoryObj<TextProps> = {}
+
+export const Small: StoryObj<TextProps> = {
+  args: {
+    size: 'sm'
+  }
+}
+
+export const Large: StoryObj<TextProps> = {
+  args: {
+    size: 'lg'
+  }
+}
+
+export const CustomComponent: StoryObj<TextProps> = {
+  args: {
+    asChild: true,
+    children: (
+      <p>Testando</p>
+    )
+  }
+}
